c5ffa12a866f0d75d6c7bfce531a2ba50e9efe04
EderSampayo <101659210+EderSampayo@users.noreply.github.com>
2022-11-08 22:30:00 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,6 +2,6 @@
 hoy=$(date +%F)
 ayer=$(date -d "yesterday" +%F)
 
-mkdir /var/tmp/Backups2/$hoy
+mkdir /var/tmp/Backups/$hoy
 
-rsync -av --link-dest=/var/tmp/Backups/$ayer /home/$(whoami)/Escritorio/Seguridad/ /var/tmp/Backups2/$hoy
+rsync -av --link-dest=/var/tmp/Backups/$ayer /home/$(whoami)/Escritorio/Seguridad/ /var/tmp/Backups/$hoy
